9867c507608644d0c9496d8e77f64c21551c1f71
inileshsharma <108680890+inileshsharma@users.noreply.github.com>
2024-08-02 19:47:08 +0530

    Maximum_Product_Subarray.cpp

--- DP/Maximum_Product_Subarray.cpp
+++ DP/Maximum_Product_Subarray.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
 
+        double maxp = nums[0];
+        double minp = nums[0];
+
+        double res = nums[0];
+
+        for(int i=1;i<nums.size();i++){
+
+            if(nums[i] < 0){
+                swap(maxp,minp);
+            }
+
+            maxp = max((double)nums[i],nums[i]*maxp);
+            minp = min((double)nums[i],nums[i]*minp);
+
+            
+            if(res < maxp) res = maxp;
+
+        }
+        return (int)res;
+}
+};
